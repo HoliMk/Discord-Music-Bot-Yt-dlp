3d15005a988d8a558355fb4ec24480cf448a563c
HoliMk <147618129+HoliMk@users.noreply.github.com>
2023-10-19 14:08:14 +0300

    Add files via upload

---
+++ Discord_music_bot/main.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+import os
+
+#import all of the cogs
+from help_cog import help_cog
+from music_cog import music_cog
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.voice_states = True
+intents.presences = True
+
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+#remove the default help command so that we can write out own
+bot.remove_command('help')
+
+#register the class with the bot
+bot.add_cog(help_cog(bot))
+bot.add_cog(music_cog(bot))
+
+#start the bot with our token
+bot.run("Your discord token!")
